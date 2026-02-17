89f0f250b46b6c2577a4afdb1957bfd612360ea5
hashir-suleman-meesho <hashir.suleman@meesho.com>
2026-02-17 22:33:35 +0530

    reels display api data

--- app/src/main/java/com/example/instagramclone/Ui/Adapters/reel_adapter.kt
+++ app/src/main/java/com/example/instagramclone/Ui/Adapters/reel_adapter.kt
@@ -4,10 +4,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.instagramclone.Data.Local.Entity.FeedEntity
 import com.example.instagramclone.Data.Local.Entity.ReelEntity
 import com.example.instagramclone.R
@@ -28,6 +32,11 @@ class ReelAdapter(private val onLikeClick: (ReelEntity) -> Unit) : RecyclerView.
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         val playerView: PlayerView = itemView.findViewById(R.id.exoPlayer)
+        val rvAvatar: ImageView = itemView.findViewById(R.id.profilePicReel)
+        val rvUsername: TextView = itemView.findViewById(R.id.usernameReel)
+        val rvLikeButton: Button = itemView.findViewById(R.id.likeButtonReel)
+        val rvLikeCount: TextView = itemView.findViewById(R.id.likeCountReel)
+        val rvDescription: TextView = itemView.findViewById(R.id.reelDescription)
     }
 
     override fun onCreateViewHolder(
@@ -43,7 +52,25 @@ class ReelAdapter(private val onLikeClick: (ReelEntity) -> Unit) : RecyclerView.
         holder: ViewHolder,
         position: Int
     ) {
+
         val reel = dataList[position]
+
+        holder.rvUsername.text = reel.user_name
+        holder.rvLikeCount.text = reel.like_count.toString()
+        holder.rvDescription.text = "by ${reel.user_name}"
+
+        // Load images from URL (API → Room → UI)
+        Glide.with(holder.itemView.context)
+            .load(reel.user_image)
+            .into(holder.rvAvatar)
+
+        holder.rvLikeButton.text =
+            if (reel.liked_by_user) "Liked" else "Like"
+
+        holder.rvLikeButton.setOnClickListener {
+            onLikeClick(reel)
+        }
+
         Log.d("bindingReel", "$reel")
         val player = ExoPlayer.Builder(holder.itemView.context).build()
         holder.playerView.player = player
